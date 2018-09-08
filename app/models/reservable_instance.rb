aafbaa8d1b2b095567323e2278ebff3405a45644
Eric Wright (DiscoPosse) <eric@discoposse.com>
2018-09-08 18:36:11 -0400

    basic instance_reservation framework before adding bookable gem

--- app/models/reservable_instance.rb
+++ app/models/reservable_instance.rb
@@ -1,2 +1,4 @@
 class ReservableInstance < ApplicationRecord
+	has_many :instance_reservations, :through => :instance_reservations
+	has_many :instance_reservations, :dependent => :destroy
 end
--- app/models/reservation_owner.rb
+++ app/models/reservation_owner.rb
@@ -1,2 +1,5 @@
 class ReservationOwner < ApplicationRecord
+	validates :email, presence: true, uniqueness: true
+	has_many :resevable_instances, :through => :instance_reservations
+	has_many :instance_reservations, :dependent => :destroy
 end
